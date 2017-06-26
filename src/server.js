a56986cd4ad578337a86434988a1138f1bfc96ea
Dmitri Kunin <whitemarten@gmail.com>
2017-06-26 22:40:51 +0300

    master:In progress of update

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
 'use strict';
 
 const express = require('express');
+const openInEditor = require('open-in-editor');
 const app = express();
-const openEditor = require('open-editor');
-var spawn = require('child_process').spawn;
 let editor = 'sublime';
-var fs = require('fs'),
-    out = fs.openSync('./out.log', 'a'),
-    err = fs.openSync('./out.log', 'a');
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -20,17 +16,20 @@ app.use(function(req, res, next) {
 
 app.get('/openeditor', function(req, res) {
     let { options = '' } = req.query;
-    const result = openEditor.make([ options ], { editor });
-    let cmd = '';
-    result.args.forEach(singleArg => {
-        cmd += ' "' + singleArg + '"';
-    });
-    spawn(result.bin, result.args, {
-        stdio: [ 'ignore', out, err ],
-        detached: true
-    }).unref();
-    // exec(cmd, console.log);
-    res.send('ok');
+    var editorFunc = openInEditor.configure(
+        {
+            editor
+        },
+        () => {}
+    );
+    editorFunc.open(options).then(
+        function() {
+            res.sendStatus('Success');
+        },
+        function(err) {
+            res.sendStatus(err);
+        }
+    );
 });
 
 module.exports = {
