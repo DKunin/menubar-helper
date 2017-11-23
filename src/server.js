ad34b591b23a36c905d2983907ecf2bc548a5a5c
Ivan Strelkov <iv-strelkov@yandex.ru>
2017-11-23 20:02:00 +0300

    Fix running VS code (#22)
    * Fix VS code option name for open-in-editor
    
    * Prefer command-line shortcut when running VS Code (usual way fails on Mac)

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@
 
 const express = require('express');
 const openInEditor = require('open-in-editor');
+const fs = require('fs');
 const app = express();
 let editor = 'sublime';
+
+const vsCodeCmd = '/usr/local/bin/code';
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -16,10 +19,16 @@ app.use(function(req, res, next) {
 
 app.get('/openeditor', function(req, res) {
     let { options = '' } = req.query;
-    var editorFunc = openInEditor.configure(
-        {
-            editor
-        },
+
+    const params = { editor };
+
+    // Running VS code inside electron on Mac does not work if path contains spaces. But works well with command line shortcut
+    if (editor === 'code' && fs.existsSync(vsCodeCmd)) {
+        params.cmd = vsCodeCmd;
+    }
+
+    const editorFunc = openInEditor.configure(
+        params,
         () => {}
     );
     editorFunc.open(options);
